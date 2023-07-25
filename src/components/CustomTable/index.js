85632e6b29c45e081ed5efbd59004233a76d778d
Nadeem <nadeem@passbird.co>
2023-07-25 09:09:39 +0530

    add sort and other deps

--- src/components/CustomTable/index.js
+++ src/components/CustomTable/index.js
@@ -1,13 +1,28 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Table } from "reactstrap";
-import { selectTableData } from "../../redux/store";
+import {
+  selectTableData,
+  setSortState,
+  selectSortState,
+} from "../../redux/store";
 import { getRowValueByType, toSentenceCase } from "../../utils/data-utils";
+import { LiaSortSolid } from "react-icons/lia";
 import styles from "./index.module.scss";
 
 export const CustomTable = () => {
   const tableData = useSelector(selectTableData);
+  const sortState = useSelector(selectSortState);
+  const dispatch = useDispatch();
   if (!tableData || !tableData.length) return null;
 
+  const onSort = (column) => {
+    const { sortByKey, direction } = sortState;
+    const newSortState = {
+      sortByKey: column,
+      direction: sortByKey === column && direction === "asc" ? "desc" : "asc",
+    };
+    dispatch(setSortState(newSortState));
+  };
   const columns = Object.keys(tableData[0]);
   return (
     <div className={styles.table_container}>
@@ -15,7 +30,15 @@ export const CustomTable = () => {
         <thead>
           <tr>
             {columns.map((column, idx) => (
-              <th key={idx}>{toSentenceCase(column)}</th>
+              <th key={idx}>
+                {toSentenceCase(column)}
+                <span
+                  onClick={() => onSort(column)}
+                  className={styles.sort_icon}
+                >
+                  <LiaSortSolid />
+                </span>
+              </th>
             ))}
           </tr>
         </thead>
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,10 @@ const INITIAL_APP_STATE = {
   savedQueries: [],
   currentActiveQuery: "",
   tableData: [],
+  sortState: {
+    sortByKey: "",
+    direction: "",
+  },
 };
 
 // Slice
@@ -27,17 +31,45 @@ const appSlice = createSlice({
     setTableData: (state, action) => {
       state.tableData = action.payload;
     },
+    setSortState: (state, action) => {
+      state.sortState = action.payload;
+    },
   },
 });
 
 // Actions
-export const { setActiveQuery, addSavedQuery, removeSavedQuery, setTableData } =
-  appSlice.actions;
+export const {
+  setActiveQuery,
+  addSavedQuery,
+  removeSavedQuery,
+  setTableData,
+  setSortState,
+} = appSlice.actions;
 
 //  Selectors
 export const selectSavedQueries = (state) => state.app.savedQueries;
 export const selectActiveQuery = (state) => state.app.currentActiveQuery;
-export const selectTableData = (state) => state.app.tableData;
+export const selectTableData = (state) => {
+  const { sortByKey, direction } = state.app.sortState;
+  if (sortByKey && direction) {
+    const tableDataToSort = [...state.app.tableData];
+    return tableDataToSort.sort((a, b) => {
+      let valOne = a[sortByKey];
+      let valTwo = b[sortByKey];
+      if (typeof valOne === "object") {
+        valOne = Object.values(valOne).join(", ");
+        valTwo = Object.values(valTwo).join(", ");
+      }
+      if (direction === "asc") {
+        return valOne > valTwo ? 1 : -1;
+      } else {
+        return valOne < valTwo ? 1 : -1;
+      }
+    });
+  }
+  return state.app.tableData;
+};
+export const selectSortState = (state) => state.app.sortState;
 
 // Store
 export default configureStore({
